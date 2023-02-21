722793b877f66b3cd124ff2f3d83141e39f9f5da
gugzkumar <gugzkumar@gmail.com>
2023-02-20 21:52:18 -0600

    backend cluster and s3

--- .infrastructure/aws/main.tf
+++ .infrastructure/aws/main.tf
@@ -25,22 +25,18 @@ resource "aws_s3_bucket_metric" "data_lake_bucket_metric" {
   name   = "${var.aws_prefix}-data-lake-metric"
 }
 
-# resource "aws_s3_bucket_logging" "example" {
-#   bucket = aws_s3_bucket.data_lake.id
-#   target_bucket = aws_s3_bucket.log_collection.id
-#   target_prefix = "log/"
-# }
+resource "aws_s3_bucket_logging" "data-lake-logging" {
+  bucket = aws_s3_bucket.data_lake.id
+  target_bucket = aws_s3_bucket.log_collection.id
+  target_prefix = "data-lake-logs/"
+}
 
 
-# Create a serverles Redshift cluster for the data warehouse
-# resource "aws_redshift_cluster" "data_warehouse" {
-#   cluster_identifier = "${var.aws_prefix}-data-warehouse"
-#   database_name      = "data_warehouse"
-#   master_username    = "admin"
-#   master_password    = "password"
-#   node_type          = "dc2.large"
-#   cluster_type       = "single-node"
-#   number_of_nodes    = 1
-#   skip_final_snapshot = true
-#   tags               = var.common_tags
-# }
+# ECR and ECS for Backend
+resource "aws_ecr_repository" "api-service" {
+  name = "${var.aws_prefix}-api-service"
+}
+
+resource "aws_ecs_cluster" "api-service-cluster" {
+  name = "${var.aws_prefix}-my-cluster"
+}
