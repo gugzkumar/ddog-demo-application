f8ffaf564b96695bf777e4c28acf4027738095d9
gugzkumar <gugzkumar@gmail.com>
2023-02-20 21:56:10 -0600

    add tags

--- .infrastructure/aws/main.tf
+++ .infrastructure/aws/main.tf
@@ -35,8 +35,10 @@ resource "aws_s3_bucket_logging" "data-lake-logging" {
 # ECR and ECS for Backend
 resource "aws_ecr_repository" "api-service" {
   name = "${var.aws_prefix}-api-service"
+  tags   = var.common_tags
 }
 
 resource "aws_ecs_cluster" "api-service-cluster" {
   name = "${var.aws_prefix}-my-cluster"
+  tags   = var.common_tags
 }
