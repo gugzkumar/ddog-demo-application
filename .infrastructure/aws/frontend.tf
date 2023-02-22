e713cbb344eafdcb126275e381115bf06b6542ae
gugzkumar <gugzkumar@gmail.com>
2023-02-22 16:29:12 -0600

    proper naming

--- .infrastructure/aws/frontend.tf
+++ .infrastructure/aws/frontend.tf
@@ -97,7 +97,7 @@ data "aws_iam_policy_document" "frontend_bucket_policy" {
 }
 
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
-  bucket = aws_s3_bucket.frontend_bucket_.id
+  bucket = aws_s3_bucket.frontend_bucket.id
   policy = data.aws_iam_policy_document.frontend_bucket_policy.json
 }
 
