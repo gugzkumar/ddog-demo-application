56267b83fbdbda88cd7725b31f958e89355203c8
gugzkumar <gugzkumar@gmail.com>
2023-02-23 02:47:13 -0600

    Confirm

--- .infrastructure/aws/backend.tf
+++ .infrastructure/aws/backend.tf
@@ -45,7 +45,7 @@ resource "aws_lb" "loadbalancer" {
 }
 
 resource "aws_lb_target_group" "lb_target_group" {
-  name     = "${var.aws_prefix}-api-loadbalancer-tg"
+  name     = "${var.aws_prefix}-api-lb-tg"
   port     = "80"
   protocol = "HTTP"
   vpc_id   = var.AWS_VPC_ID
